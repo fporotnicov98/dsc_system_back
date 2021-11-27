7f1de3835430ec549161221764c02d8ebf07469e
Fedor Porotnicov <f.s.porotnikov@tmn3.etagi.com>
2021-11-27 18:56:35 +0500

    fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -75,22 +75,22 @@ router.post(
 //api/auth/login
 router.post(
   '/login',
-  [
-    check('email', 'Введите корректный email').normalizeEmail().isEmail(),
-    check('password', 'вВведите пароль').exists()
-  ],
+  // [
+  //   check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+  //   check('password', 'вВведите пароль').exists()
+  // ],
   async (req, res) => {
     try {
       console.log(req);
 
-      const errors = validationResult(req)
+      // const errors = validationResult(req)
 
-      if (!errors.isEmpty) {
-        return res.status(400).json({
-          errors: errors.array(),
-          message: 'Некорректные данные при входе в систему'
-        })
-      }
+      // if (!errors.isEmpty) {
+      //   return res.status(400).json({
+      //     errors: errors.array(),
+      //     message: 'Некорректные данные при входе в систему'
+      //   })
+      // }
 
 
       const { email, password } = req.body
