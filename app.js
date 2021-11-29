3537738bcf2134647949658be2851d53bca935c1
Fedor Porotnicov <f.s.porotnikov@tmn3.etagi.com>
2021-11-30 01:52:39 +0500

    fix

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(cors({ credentials: true, origin: true }))
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/project', require('./routes/project.routes'))
+app.use('/api/projects', require('./routes/project.routes'))
+app.use('/api/users', require('./routes/user.routes'))
 
 
 const start = async () => {
