c5271c3bd29646aeba44db21835c0dcf22e539a3
Fedor Porotnicov <f.s.porotnikov@tmn3.etagi.com>
2021-11-30 00:28:07 +0500

    fix

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1]
 
     if (!token) {
-      return res.status(401).json({ message: 'Вы неавторизованны' })
+      return res.status(403).json({ message: 'Вы неавторизованны' })
     }
 
     const decoded = jwt.verify(token, config.get('jwtSecret'))
