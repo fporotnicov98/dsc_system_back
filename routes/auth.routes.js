c476d527f9266cab05115c760bcabd47a923f399
Fedor Porotnicov <f.s.porotnikov@tmn3.etagi.com>
2021-11-29 20:39:17 +0500

    fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -69,7 +69,7 @@ router.post(
 
       res.status(201).json({ user, message: "Пользователь создан", resultCode: 0 })
     } catch (e) {
-      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова', e })
     }
   }
 )
