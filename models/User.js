af05b522a8d412ed5ce9963b7f6bff3fb98e2ecc
Fedor Porotnicov <f.s.porotnikov@tmn3.etagi.com>
2021-11-30 01:11:45 +0500

    fix

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const schema = new Schema({
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true},
+  role: { type: String},
   owner: {type: Types.ObjectId, ref: 'SA'}
 })
 
